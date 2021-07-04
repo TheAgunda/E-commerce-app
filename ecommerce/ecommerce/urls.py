8512c01d7d8d4b210163884c7732ceca68d5fe26
badolakiran96 <kiran.kumar@hachiweb.com>
2021-07-04 16:55:28 +0530

    Create serializers and product api

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -22,5 +22,6 @@ from django.conf import settings
 urlpatterns = [
     path("api/v1/", include("djoser.urls")),
     path("api/v1/", include("djoser.urls.authtoken")),
+    path("api/v1/", include("product.urls")),
     path("admin/", admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
