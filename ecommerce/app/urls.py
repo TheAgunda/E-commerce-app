92792700192e35bc736077c56fc5de8d18ede375
badolakiran96 <kiran.kumar@hachiweb.com>
2021-07-04 16:05:01 +0530

    Setup media root and url

--- ecommerce/app/urls.py
+++ ecommerce/app/urls.py
@@ -1,9 +1,6 @@
-from django.contrib import admin
 from django.urls import path, include
 
 
 urlpatterns = [
-    path("api/v1/", include("djoser.urls")),
-    path("api/v1/", include("djoser.urls.authtoken")),
-    path("admin/", admin.site.urls),
+ 
 ]
--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, include
+from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
-    path("", include("app.urls")),
-]
+    path("api/v1/", include("djoser.urls")),
+    path("api/v1/", include("djoser.urls.authtoken")),
+    path("admin/", admin.site.urls),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
