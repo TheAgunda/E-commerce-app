8ec9d4ebc94a9bc7dda5d19aad2bfcecc894e416
badolakiran96 <kiran.kumar@hachiweb.com>
2021-07-21 21:57:26 +0530

    Make Order Migrations

--- ecommerce/order/models.py
+++ ecommerce/order/models.py
@@ -4,18 +4,18 @@ from product.models import Product
 
 class Order(models.Model):
     user = models.ForeignKey(User, related_name = "orders", on_delete = models.CASCADE)
-    first_name = models.CharFields(max_length=100)
-    last_name = models.CharFields(max_length=100)
-    email = models.CharFields(max_length=100)
-    address = models.CharFields(max_length=100)
-    zipcode = models.CharFields(max_length=100)
-    place = models.CharFields(max_length=100)
-    phone = models.CharFields(max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    zipcode = models.CharField(max_length=100)
+    place = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
     created_at =models.DateTimeField(auto_now_add = True)
-    paid_amound = models.DecimalField(max_digit=8, decimal_phaces=2, black=True, null = True)
+    paid_amound = models.DecimalField(max_digits=8, decimal_places=2, blank=True, null = True)
     stripe_token = models.CharField(max_length = 150)
     
-    def Meta:
+    class Meta:
         ordering = ['-created_at']
     def __str__(self):
         return self.first_name
@@ -23,8 +23,8 @@ class Order(models.Model):
 class OrderItem(models.Model):
     order =  models.ForeignKey(Order, related_name = "items", on_delete = models.CASCADE)
     product = models.ForeignKey(Product, related_name = "items", on_delete = models.CASCADE)
-    price = models.DecimalField(max_digit=8, decimal_phaces=2, black=True, null = True)
+    price = models.DecimalField(max_digits=8, decimal_places=2, blank=True, null = True)
     quantity = models.IntegerField(default = 1)
     
     def __str__(self):
-        return '%s' %s self.id
+        return '%s' % self.id
