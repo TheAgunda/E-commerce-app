569c5589f8438f23a9781a20c6d07a6e00fc3d28
badolakiran96 <kiran.kumar@hachiweb.com>
2021-07-28 22:19:55 +0530

    Create My Orders component in my account page

--- ecommerce/order/serializers.py
+++ ecommerce/order/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from . models import Order, OrderItem
+from .models import Order, OrderItem
 
 from product.serializers import ProductSerializers
 
@@ -13,27 +13,61 @@ class OrderItemSerializer(serializers.ModelSerializer):
             "product",
             "quantity",
         )
+
+
 class OrderSerializers(serializers.ModelSerializer):
     items = OrderItemSerializer(many=True)
+
     class Meta:
         model = Order
         fields = (
-            'id',
-            'first_name',
-            'last_name',
-            'email',
-            'address',
-            'zipcode',
-            'place',
-            'phone',
-            'stripe_token',
-            'items'
-            )
+            "id",
+            "first_name",
+            "last_name",
+            "email",
+            "address",
+            "zipcode",
+            "place",
+            "phone",
+            "stripe_token",
+            "items",
+        )
+
     def create(self, validated_data):
-        items_data = validated_data.pop('items')
+        items_data = validated_data.pop("items")
         print(items_data)
         order = Order.objects.create(**validated_data)
         for item_data in items_data:
             OrderItem.objects.create(order=order, **item_data)
         return order
-        
+
+
+class MyOrderItemSerializer(serializers.ModelSerializer):
+    product = ProductSerializers()
+
+    class Meta:
+        model = OrderItem
+        fields = (
+            "price",
+            "product",
+            "quantity",
+        )
+
+
+class MyOrderSerializers(serializers.ModelSerializer):
+    items = MyOrderItemSerializer(many=True)
+    class Meta:
+        model = Order
+        fields = (
+            "id",
+            "first_name",
+            "last_name",
+            "email",
+            "address",
+            "zipcode",
+            "place",
+            "phone",
+            "stripe_token",
+            "items",
+            # "paid_amount",
+        )
--- ecommerce/order/views.py
+++ ecommerce/order/views.py
@@ -5,32 +5,47 @@ from django.http import Http404
 from django.shortcuts import render
 from rest_framework import status, authentication, permissions
 
-from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.decorators import (
+    api_view,
+    authentication_classes,
+    permission_classes,
+)
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from . models import Order, OrderItem
-from . serializers import OrderSerializers
+from .models import Order, OrderItem
+from .serializers import OrderSerializers, MyOrderSerializers
 
 
-@api_view(['POST'])
+@api_view(["POST"])
 @authentication_classes([authentication.TokenAuthentication])
 @permission_classes([permissions.IsAuthenticated])
-def checkout(request): 
+def checkout(request):
     serializer = OrderSerializers(data=request.data)
     if serializer.is_valid():
         stripe.api_key = settings.STRIPE_SECRET_KEY
-        paid_amound = sum(item.get('quantity')* item.get('product').price for item in serializer.validated_data['items'])
+        paid_amound = sum(
+            item.get("quantity") * item.get("product").price
+            for item in serializer.validated_data["items"]
+        )
         try:
             # charge = stripe.Charge.create(
             #     amount= int(paid_amound *100),
-            #     currency = 'USD',    
-            #     description = "Charge from E-Commerce",         
+            #     currency = 'USD',
+            #     description = "Charge from E-Commerce",
             #     source =   serializer.validated_data['stripe_token']
             # )
-            serializer.save(user= request.user, paid_amound = paid_amound)
+            serializer.save(user=request.user, paid_amound=paid_amound)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         except Exception:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-            
+
+
+class OrdersList(APIView):
+    @authentication_classes([authentication.TokenAuthentication])
+    @permission_classes([permissions.IsAuthenticated])
+    def get(self, request, format=None):
+        orders = Order.objects.filter(user=request.user)
+        serializer = MyOrderSerializers(orders, many=True)
+        return Response(serializer.data)
